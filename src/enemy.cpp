aaa6d5aa9b58fa7a5f99e509013ec6a53edebdc6
luca <luca.kredel@web.de>
2019-09-11 21:02:32 +0200

    Change enemy radius

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -2,7 +2,7 @@
 
 namespace sp9k {
 Enemy::Enemy(sf::Vector2f position, sf::Vector2f velocity)
-    : Entity(position, velocity, 50) {}
+    : Entity(position, velocity, 22) {}
 
 void Enemy::update(float dt) { position += velocity * dt; }
 } // namespace sp9k
